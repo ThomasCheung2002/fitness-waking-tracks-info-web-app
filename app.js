a77400a6c2f2cdae7d6655f1f8cf92db306c6d96
ThomasCheung2002 <125951168+ThomasCheung2002@users.noreply.github.com>
2023-04-04 21:28:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,46 @@ function showDetail(id) {
 }
 
 //read data.json
-const request = new XMLHttpRequest();
-request.open('GET', 'data.json', false); 
-request.send(null);
+function retrieveTrails() {
+    const xhr = new XMLHttpRequest();
+    const url = "trail.json";
 
-if (request.status === 200) {
-  var tracksData = JSON.parse(request.responseText);
-  console.log(tracksData);
-} else {
-  console.error('Error:', request.status);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var trails = JSON.parse(xhr.response).trails;
+            displayTrails(trails);
+
+            // Store contact data to localstorage
+            const localStorage = window.localStorage;
+            if (localStorage) {
+                localStorage.setItem("trails", JSON.stringify(trails));
+            }
+        }
+    };
+
+    xhr.open("get", url);
+    xhr.send();
+}
+
+function displayTrails(trails) {
+    trails.forEach(addRow);
+}
+
+function addRow(trail) {
+    var tcontent = document.getElementById("tcontent");
+    var row = tcontent.insertRow();
+
+    var nameCell = row.insertCell();
+    nameCell.setAttribute('data-label', "English");
+    nameCell.innerHTML = trail.Title_en;
+
+    var addressCell = row.insertCell();
+    addressCell.setAttribute('data-label', "Chinese (Traditional)");
+    addressCell.innerHTML = trail.Title_tc;
+
+    var mobileCell = row.insertCell();
+    mobileCell.setAttribute('data-label', "Chinese (Simplified)");
+    mobileCell.innerHTML = trail.Title_sc;
 }
 
 tracksData.forEach((track,trackListIndex) => {
